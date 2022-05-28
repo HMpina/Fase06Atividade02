9142c1ca3c32524583877d662a7a6d4a94a9368e
HMpina <henrique.marques.pina@gmail.com>
2022-05-27 22:36:04 -0300

    Rest

--- src/main/java/com/traveller/controller/AcomodacaoController.java
+++ src/main/java/com/traveller/controller/AcomodacaoController.java
@@ -3,19 +3,18 @@ package com.traveller.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.traveller.model.Acomodacao;
-import com.traveller.model.Traveller;
 import com.traveller.repository.AcomodacaoRepository;
 
 	
-	@Controller
+	@RestController
 	@RequestMapping("/")
 	public class AcomodacaoController {
 
