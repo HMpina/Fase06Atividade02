06cfbf5e5a4da1b297ac8847ec08f8b39b338332
HMpina <henrique.marques.pina@gmail.com>
2022-05-29 15:28:31 -0300

    sevices

--- src/main/java/com/traveller/controller/TravellerController.java
+++ src/main/java/com/traveller/controller/TravellerController.java
@@ -3,7 +3,7 @@ package com.traveller.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.traveller.model.Acomodacao;
+//import com.traveller.model.Acomodacao;
 import com.traveller.model.Traveller;
 import com.traveller.repository.TravellerRepository;
 
