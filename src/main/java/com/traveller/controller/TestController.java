401b6d36ebb4fd0873ca35384fe5f7a2df0db4c6
HMpina <henrique.marques.pina@gmail.com>
2022-05-29 16:04:15 -0300

    services

--- src/main/java/com/traveller/controller/TestController.java
+++ src/main/java/com/traveller/controller/TestController.java
@@ -1,8 +1,9 @@
 package com.traveller.controller;
 
-import com.traveller.dtos.AcomodacaoDto;
-import com.traveller.model.Acomodacao;
-import com.traveller.services.AcomodacaoService;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,10 +11,19 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.util.Optional;
+import com.traveller.dtos.AcomodacaoDto;
+import com.traveller.model.Acomodacao;
+import com.traveller.services.AcomodacaoService;
 
 
 @RestController
@@ -29,7 +39,7 @@ public class TestController {
 
     @PostMapping
     public ResponseEntity<Object> saveAcomodacao(@RequestBody @Valid AcomodacaoDto acomodacaoDto){
-        var acomodacao = new Acomodacao();
+    	Acomodacao acomodacao = new Acomodacao();
         BeanUtils.copyProperties(acomodacaoDto, acomodacao);
         return ResponseEntity.status(HttpStatus.CREATED).body(acomodacaoService.save(acomodacao));
     }
@@ -65,7 +75,7 @@ public class TestController {
         if (!acomodacaoOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
         }
-        var acomodacao = new Acomodacao();
+        Acomodacao acomodacao = new Acomodacao();
         BeanUtils.copyProperties(acomodacaoDto, acomodacao);
         acomodacao.setCodAcomodacao(acomodacaoOptional.get().getCodAcomodacao());
                 return ResponseEntity.status(HttpStatus.OK).body(acomodacaoService.save(acomodacao));
