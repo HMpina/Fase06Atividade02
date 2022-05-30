4a76cc190df4dcfc0c752ad6ae1cbb5115daeaa2
HMpina <henrique.marques.pina@gmail.com>
2022-05-29 21:40:46 -0300

    html

--- src/main/java/com/traveller/controller/TravellerController.java
+++ src/main/java/com/traveller/controller/TravellerController.java
@@ -1,5 +1,34 @@
 package com.traveller.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.traveller.model.AcomodacaoModel;
+import com.traveller.repository.AcomodacaoRepository;
+
 public class TravellerController {
 
+	
+	@Controller
+	@RequestMapping("/")
+	public class AcomodacaoController {
+
+		@Autowired
+	    private AcomodacaoRepository acomodacaoRepository;
+
+
+
+	    @GetMapping("listaAcomodacao")
+		public String listaAcomodacao(Model request) {
+			List<AcomodacaoModel> lista = acomodacaoRepository.findAll();
+			request.addAttribute("listaAcomodacao", lista);	
+			return "listaAcomodacao";
+	    }
+	
+	}
 }
